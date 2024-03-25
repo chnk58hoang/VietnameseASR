4f77680fda8e21e4bc6e0b1e1c9eb2ffb889ae5c
chnk58hoang <19021285@vnu.edu.vn>
2024-03-25 22:53:52 +0700

    get tar files and manifest files

--- train.py
+++ train.py
@@ -1,54 +1,12 @@
-from omegaconf import OmegaConf, open_dict
+from utils import load_config, build_model
 import pytorch_lightning as pl
-import nemo.collections.asr as nemo_asr
 import argparse
-
-
-def load_config(args):
-    config = OmegaConf.load(args.config)
-    config.model.train_ds.manifest_filepath = args.train_manifest
-    config.model.validation_ds.manifest_filepath = args.valid_manifest
-    config.model.test_ds.manifest_filepath = args.test_manifest
-
-    config.model.train_ds.is_tarred = args.is_tarred
-    config.model.train_ds.tarred_path = args.tarred_path
-
-    config.model.tokenizer.dir = args.tokenizer_dir
-    config.trainer.max_epochs = args.epochs
-    config.trainer.accumulate_grad_batches = args.acc_grad
-    config.trainer.precision = args.precision
-
-    config.exp_manager.resume_from_checkpoint = args.resume
-
-    if args.model_size == 'medium':
-        config.model.encoder.n_heads = 4
-        config.model.encoder.n_layers = 18
-        config.model.decoder.d_model = 256
-    elif args.model_size == 'large':
-        config.model.encoder.n_heads = 8
-        config.model.encoder.n_layers = 18
-        config.model.decoder.d_model = 512
-    elif args.model_size == 'xlarge':
-        config.model.encoder.n_heads = 8
-        config.model.encoder.n_layers = 24
-        config.model.decoder.d_model = 1024
-    elif args.model_size == 'small':
-        config.model.encoder.n_heads = 4
-        config.model.encoder.n_layers = 16
-        config.model.decoder.d_model = 176
-
-    return config
-
-
-def build_model(config):
-    model = nemo_asr.models.EncDecCTCModel(cfg=config.model)
-    return model
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate manifest file')
     parser.add_argument('--config', type=str, required=True, help='path to the config file')
-    parser.add_argument('--train_manifest', type=str, required=True, help='path to the training manifest file')
+    parser.add_argument('--data_dir', type=str, required=True, help='path to the tarred data directory')
     parser.add_argument('--valid_manifest', type=str, required=True, help='path to the valid manifest file')
     parser.add_argument('--test_manifest', type=str, required=True, help='path to the test manifest file')
     parser.add_argument('--is_tarred', type=bool, required=True, help='whether the dataset is tarred')
---
+++ utils.py
@@ -0,0 +1,80 @@
+from omegaconf import OmegaConf
+import nemo.collections.asr.models as models
+from typing import List
+import os
+
+
+def get_all_tar_files(data_dir: str,
+                      num_shards: int) -> List[List[str]]:
+    """get path to all .tar files
+
+    Args:
+        data_dir (str): path to the data directory
+        num_shards (int): number of shards
+
+    Returns:
+        List[List[str]]: list of paths to .tar files. Each file path in a sub-list
+    """
+    all_tar_files = []
+    for subdir in os.listdir(data_dir):
+        tar_file = os.path.join(data_dir, subdir, f'audio__OP_1..{num_shards-1}_CL_.tar')
+        all_tar_files.append([tar_file])
+    return all_tar_files
+
+
+def get_all_manifest_files(data_dir: str) -> List[List[str]]:
+    """get path to all .json files
+
+    Args:
+        data_dir (str): path to the data directory
+        num_shards (int): number of shards
+
+    Returns:
+        List[List[str]]: list of paths to .json files. Each file path in a sub-list
+    """
+    all_manifest_files = []
+    for subdir in os.listdir(data_dir):
+        manifest_file = os.path.join(data_dir, subdir, f'tarred_audio_manifest.json')
+        all_manifest_files.append([manifest_file])
+    return all_manifest_files
+
+
+def load_config(args):
+    config = OmegaConf.load(args.config)
+    config.model.train_ds.manifest_filepath = get_all_manifest_files(args.data_dir)
+    config.model.validation_ds.manifest_filepath = args.valid_manifest
+    config.model.test_ds.manifest_filepath = args.test_manifest
+
+    config.model.train_ds.is_tarred = args.is_tarred
+    config.model.train_ds.tarred_audio_filepaths = get_all_tar_files(args.data_dir, args.num_shards)
+
+    config.model.tokenizer.dir = args.tokenizer_dir
+    config.trainer.max_epochs = args.epochs
+    config.trainer.accumulate_grad_batches = args.acc_grad
+    config.trainer.precision = args.precision
+    config.exp_manager.resume_from_checkpoint = args.resume
+
+    if args.model_size == 'medium':
+        config.model.encoder.n_heads = 4
+        config.model.encoder.n_layers = 18
+        config.model.decoder.d_model = 256
+    elif args.model_size == 'large':
+        config.model.encoder.n_heads = 8
+        config.model.encoder.n_layers = 18
+        config.model.decoder.d_model = 512
+    elif args.model_size == 'xlarge':
+        config.model.encoder.n_heads = 8
+        config.model.encoder.n_layers = 24
+        config.model.decoder.d_model = 1024
+    elif args.model_size == 'small':
+        config.model.encoder.n_heads = 4
+        config.model.encoder.n_layers = 16
+        config.model.decoder.d_model = 176
+
+    return config
+
+
+def build_model(config):
+    model = models.EncDecCTCModelBPE(cfg=config.model)
+    return model
+
